a5ce721371a1cb7a05d8f4b2c288bf44f42b585b
Chaïner KOMISSA ZOTSU <essikomissa@gmail.com>
2023-10-05 22:22:03 +0000

    Commit 2
    Design de la page menu

--- my-app/src/screens/menu.jsx
+++ my-app/src/screens/menu.jsx
@@ -1,19 +1,82 @@
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { MenuOutlined, SearchOutlined } from '@ant-design/icons';
 
 const styles = StyleSheet.create({
+  container : {
+    width : '100%',
+    height : '100%',
+    flexDirection : 'column',
+  },
   row : {
       width : '100%',
-      backgroundColor : '#F1F1E6'
-  }
+      flexDirection : 'row',
+      height : 50,
+      marginTop : 80,
+  },
+  texte : {
+      fontSize : 30,
+      fontWeight : 'bold',
+      paddingLeft : 10
+  },
+  blue : {
+    margin : 20,
+    width : '90%',
+    height : '300px',
+    backgroundColor : '#0552AC',
+    borderRadius : 15
+  },
+  yellow : {
+    margin : 20,
+    width : '90%',
+    height : '300px',
+    backgroundColor : '#E3B203',
+    borderRadius : 15
+  },
+  textebtnblue : {
+    marginTop : 80,
+    marginLeft : 20,
+    marginBottom : 20,
+    color : '#fff',
+    fontSize : 20,
+    fontWeight : 'bold',
+  },
+  textebtnyellow : {
+    marginTop : 80,
+    marginLeft : 20,
+    marginBottom : 20,
+    color : '#0552AC',
+    fontSize : 20,
+    fontWeight : 'bold',
+  },
 })
 
 const Menu = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.row}>
-        <Text>Test</Text>
+          <Text style={styles.texte}>Menu</Text>
       </View>
+      <ScrollView>
+      <View style={styles.blue}>
+        <Text style={styles.textebtnblue}>Irregular Verbs</Text>
+      </View>
+      <View style={styles.yellow}>
+        <Text style={styles.textebtnyellow}>Vocabulary</Text>
+      </View>
+      <View style={styles.blue}>
+        <Text style={styles.textebtnblue}>Grammar</Text>
+      </View>
+      <View style={styles.yellow}>
+        <Text style={styles.textebtnyellow}>Jumble words</Text>
+      </View>
+      <View style={styles.blue}>
+        <Text style={styles.textebtnblue}>General Knowledge</Text>
+      </View>
+      <View style={styles.yellow}>
+        <Text style={styles.textebtnyellow}>Quiz</Text>
+      </View>
+      </ScrollView>
     </View>
   )
 }
